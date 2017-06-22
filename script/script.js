0b49dffff6a8d7e5a6a9c87e847d7ce49458f5d6
Paladines <Stephen_Paladines@Dell.com>
2017-06-22 01:35:52 -0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+
+console.log(2+2);
+
+var input = prompt("Please enter a number");
+
+switch(input){
+  case "1":
+  console.log("Helllllooooooo");
+  break;
+  case "2":
+  console.log("Hellllloo4444ooooo");
+  break;
+  case "3":
+  console.log("Helllll2424242ooooooo");
+  break;
+}
